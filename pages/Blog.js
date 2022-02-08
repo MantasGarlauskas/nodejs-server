01d50d986b798e2e370fa49a14773eec698f26df
Mantas Garlauskas <mantas.garlauskas@gmail.com>
2022-02-08 18:29:19 +0200

    min height for the content part

--- pages/Blog.js
+++ pages/Blog.js
@@ -37,7 +37,10 @@ class PageBlog extends PageTemplate {
             HTML += this.blogPostHTML(item);
         }
 
-        return `<div class="row list">${HTML}</div>`;
+        return `<div class="row list">${HTML}</div>
+        <div class="row">
+                        BLOG PAGINATION
+                    </div>`;
     }
 
     mainHTML() {
@@ -47,9 +50,7 @@ class PageBlog extends PageTemplate {
         return `<section class="container blog-list">
                     <h1 class="row title">My blog</h1>
                     ${contentHTML}
-                    <div class="row">
-                        BLOG PAGINATION
-                    </div>
+                    
                 </section>`;
     }
 }
